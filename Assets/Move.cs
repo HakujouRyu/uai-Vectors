e2291fbbe4abfbc76e1b9b5bf0835c4359404050
hakujouryu <hakujouryu@gmail.com>
2020-03-26 09:30:27 -0400

    complete

---
+++ Assets/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    // Vector3 goal = new Vector3(5, 0, 4);
+ public float speed = 2.0f;
+    public Transform goal;
+
+    void Start() {
+
+        // this.transform.Translate(goal);
+    }
+
+    private void Update() {
+        Vector3 direction = goal.position - this.transform.position;
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime );
+    }
+}
